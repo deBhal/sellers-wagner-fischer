9ac365c07ed56b34b0a171698377c78d9547ba81
Julian de Bhal <julian.debhal@automattic.com>
2016-03-21 09:37:28 +1000

    eslint --fix

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ http://www.di.ufpe.br/~paguso/courses/if767/bib/Sellers_1980.pdf
 var _ = require('lodash');
 
 
-module.exports = function( string, substring ){
-	var matrix = [_.map(string,function(){return 0;})];
+module.exports = function( string, substring ) {
+	var matrix = [_.map(string, function() {return 0;})];
 	var stringAsArray = string.split('');
 	var substringAsArray = substring.split('');
 	var carry, result;
@@ -16,8 +16,8 @@ module.exports = function( string, substring ){
 		matrix.push(
 			_.zipWith(
 				_.last(matrix), _.dropRight(_.concat(Infinity, _.last(matrix))), stringAsArray,
-				function (n, upLeft, l) {
-					return carry = Math.min(n,carry,upLeft) + ((letter === l) ?  0 : 1);
+				function(n, upLeft, l) {
+					return carry = Math.min(n, carry, upLeft) + ((letter === l) ? 0 : 1);
 				}));
 	});
 
