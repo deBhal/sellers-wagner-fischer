dbc3855833b8deec15e72db7598ecce58d639440
Julian de Bhal <julian.debhal@automattic.com>
2016-03-21 09:40:39 +1000

    eslint fixes

--- index.js
+++ index.js
@@ -2,24 +2,30 @@
 http://www.di.ufpe.br/~paguso/courses/if767/bib/Sellers_1980.pdf
 */
 
-var _ = require('lodash');
-
+var zipWith = require( 'lodash/zipWith' );
+var each = require( 'lodash/each' );
+var last = require( 'lodash/last' );
+var dropRight = require( 'lodash/dropRight' );
+var concat = require( 'lodash/concat' );
+var map = require( 'lodash/map' );
 
 module.exports = function( string, substring ) {
-	var matrix = [_.map(string, function() {return 0;})];
-	var stringAsArray = string.split('');
-	var substringAsArray = substring.split('');
-	var carry, result;
+	var matrix = [ map( string, function() {
+		return 0;
+	} ) ];
+	var stringAsArray = string.split( '' );
+	var substringAsArray = substring.split( '' );
+	var carry;
 
-	_.each(substringAsArray, function(letter) {
+	each( substringAsArray, function( letter ) {
 		carry = Infinity;
 		matrix.push(
-			_.zipWith(
-				_.last(matrix), _.dropRight(_.concat(Infinity, _.last(matrix))), stringAsArray,
-				function(n, upLeft, l) {
-					return carry = Math.min(n, carry, upLeft) + ((letter === l) ? 0 : 1);
-				}));
-	});
+			zipWith(
+				last( matrix ), dropRight( concat( Infinity, last( matrix ) ) ), stringAsArray,
+				function( n, upLeft, l ) {
+					return ( carry = Math.min( n, carry, upLeft ) + ( ( letter === l ) ? 0 : 1 ) );
+				} ) );
+	} );
 
-	return _.last(matrix);
-}
+	return last( matrix );
+};
